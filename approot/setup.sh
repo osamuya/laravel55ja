76d0dbe739c969c725c4b3a4e67821d5350130a3
osamuya <oosamuuy@gmail.com>
2018-04-18 14:36:15 +0900

    最適化ツールとセットアップツール

---
+++ approot/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+chmod 777 $FULLPATH'bootstrap/cache'
+chmod 666 bootstrap/cache/services.php
+chmod 666 storage/logs/laravel.log
+chmod 777 storage/
+chmod 777 storage/logs
+chmod 777 storage/framework/views
+chmod 777 storage/framework/sessions
+chmod 777 storage/debugbar
