231dbe7e86cb73b1c3418c5632226a88a3726890
osamuya <oosamuuy@gmail.com>
2018-04-18 22:59:00 +0900

    認証テーブルの作成（オリジナル）

--- .doc/Order.md
+++ .doc/Order.md
@@ -3,3 +3,5 @@
 1. 最適化・セットアップツール
 1. DB作成と接続
 1. migrate (utf8mb4_general_ci)
+1. app/Providers/AppServiceProvider.php (utf8mb4のmigrate error対策)
+1. 認証テーブルの作成（オリジナル）
--- approot/app/User.php
+++ approot/app/User.php
@@ -8,14 +8,44 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+	
+    /**
+     * モデルと関連しているテーブル
+     *
+     * @var string
+	 *
+        id 主 int(10)
+        name varchar(128)
+        password varchar(256)
+        email varchar(255)
+        uniqueid varchar(64)
+        uniquehash varchar(64)
+        remember_token varchar(100)
+        description longtext
+        role int(11)
+        status int(11)
+        delflag tinyint(3)
+        created_at timestamp
+        updated_at timestamp
+	 *
+     */
+    protected $table = 'base_users';
+	
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'password',
+        'email',
+		'uniqueid',
+        'uniquehash',
+        'description',
+        'role',
+        'status',
+        'delflag',
     ];
 
     /**
@@ -24,6 +54,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'uniqehash',
     ];
 }
