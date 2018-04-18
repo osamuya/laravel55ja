f116d17419d8d373f9a2877888dced49143cfc09
osamuya <oosamuuy@gmail.com>
2018-04-18 23:53:19 +0900

    メール認証のコントローラとビューの作成(2)

--- approot/app/Http/Controllers/Auth/RegisterController.php
+++ approot/app/Http/Controllers/Auth/RegisterController.php
@@ -71,14 +71,22 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        /*入力データを返す*/
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => $data['password'],
+            'uniqueid' => $data['uniqueid'],
+            'uniquehash' => $data['uniquehash'],
+			#'remember_token' => "",
+			#'description' => $data['description'],
+            'role' => $data['role'],
+            'status' => $data['status'],
+            'delflag' => $data['delflag'],
         ]);
     }
-	
-	
+
+
     /**
      * Confirm page to create a new user
      *
@@ -86,39 +94,135 @@ class RegisterController extends Controller
      * @return view("auth.register_confirm")
      */
 	protected function registConfirm(Request $request) {
-		
+
         /* Validation */
         $this->validate($request, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:base_users',
             'password' => 'required|string|min:6|confirmed',
         ]);
-		
+
         /* Request data */
         $requestData = array(
             'name' => $request->input('name'),
             'email' => $request->input('email'),
             'password' => $request->input('password'),
         );
-		
+
         /* Session flash keep */
         $request->session()->flash("name", $request->input('name'));
         $request->session()->flash("email", $request->input('email'));
         $request->session()->flash("password", $request->input('password'));
-		
+
 		/* Reload prevention request and session */
         $request->session()->flash('newRegistStoreFlag',true);
-		
+
 		return view("auth.register_confirm")->with($requestData);
 	}
 	
 	
+    /**
+     * Store page to new user
+     *
+     * @param  Request [ session ]
+     * @return view("auth.register_stored")
+     */
+	protected function registStore(Request $request) {
+		
+		if ($request->session()->get('newRegistStoreFlag') === true) {
+			/* Go! */
+		} else {
+			return redirect('/');
+		}
+		
+		/* Password hash */
+		$passwordHash = bcrypt($request->session()->get("password"));
+		
+		/* uniqeid */
+		$uniqeid = $this->getUniqeid("hoge");
+		
+		/* Uniqehash */
+		$uniquehash = substr(hash('sha512',$request->session()->get("password").env("APP_KEY").date("Ymdist")),0,60);
+
+		/* Save regist data */
+        $data = array(
+            'name' => $request->session()->get("name"),
+            'email' => $request->session()->get("email"),
+            'password' => $passwordHash,
+            'uniqueid' => $uniqeid,
+            'uniquehash' => $uniquehash,
+            'role' => 1,
+            'status' => 1,
+            'delflag' => 0,
+        );
+		$this->create($data);
+
+		/* Preparation for mail authentication */
+		$options = [
+			'from' => 'from@example.com',
+			'from_jp' => '【 '.env("APP_NAME").'より 会員仮登録のお知らせ 】',
+			'to' => $request->session()->get("email"),
+			'subject' => env("APP_NAME").'より 会員仮登録のお知らせ',
+			'template' => 'mails.regist.temp_welcome',
+			"bcc" => env("ADMIN_MAIL"),
+		];
+		
+		/* Parameter for mail auth */
+		/* Make access URL */
+		$makeURL = env("APP_URL")."/".env("APP_NAME")."/mail_authenticate_user/".$uniquehash;
+		/* Get global IP address */
+        $userip = \Request::ip();
+		/* Create a password of a bullet on a bullet */
+		$boundLetteredPassword = $this->boundLettered($request->session()->get("password"),1);
+		/* Date time */
+        $dt = Carbon::now();
+        $registedDate =  $dt->format('Y年m月d日 h:i:s');
+		
+		$emaildata = [
+			"apprication_name" => env("APP_NAME"),
+			"user_name" => $request->session()->get("name"),
+			'user_email' => $request->session()->get("email"),
+			'user_password' => $boundLetteredPassword,
+			'uniquehash' => $uniquehash,
+			'makeurl' => $makeURL,
+			'registed_date' => $registedDate,
+			'registed_term' => 24,
+			'url' => env("APP_URL"),
+		];
+
+		Mail::to($request->session()->get("email"))->send(new BaseMail($options, $emaildata));
+		
+		/* No reloading */
+		$request->session()->forget('newRegistStoreFlag');
+		
+		return view("auth.register_stored");
+	}
+	
+	protected function getUniqeid($salt) {
+
+		$uniqid = implode("-", str_split(strtoupper(uniqid("00")), 3));
+		$uniqid = "ID-".$uniqid;
+		return $uniqid;
+	}
 	
 	
-	
-	
-	
-	
+	public function boundLettered($strings, $fchara=0) {
+
+        $displayChara = substr($strings, 0,$fchara);
+
+        $count = mb_strlen($strings);
+        $count = $count - $fchara;
+        $boundLettered = "";
+        for($i=0; $i<$count; $i++) {
+            $boundLettered .= "*";
+        }
+        if ($fchara > 0) {
+            $returnBoundLettered = $displayChara.$boundLettered;
+        } else if ($fchara == 0) {
+            $returnBoundLettered = $boundLettered;
+        }
+        return $returnBoundLettered;
+    }
 	
 	
 	
